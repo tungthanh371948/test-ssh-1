5eaa4b2129b2035b525a1f036d4f217b79c759a1
thanh tung <tungthanh371948@gmail.com>
2023-12-22 22:04:03 +0700

    update video 40

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,11 @@
 const connection = require('../config/database');
 
-const getHomepage =(req,res)=>{
-   return res.render('home.ejs')
+const getHomepage = async  (req,res)=>{
+    let [result, fields] = await connection.query('select * from Users') 
+    console.log(">>check query", result)
+   return res.render('home.ejs', {ListUsers:result})
 }
-  
+
 const getABC = (req,res) => {
     res.send('Hoi tung dote come')
 }
@@ -15,18 +17,14 @@ const postCreateUsers = async(req,res) =>{
     let email = req.body.email;
     let name = req.body.myname;
     let city = req.body.city;
-    console.log(">>> emai = ", email , "name = ", name , "city = ", city)
-
-  console.log(">>> request.body: ", req.body)
+    console.log(">>> request.body: ", req.body)
 
   let [results, fields] = await connection.query(
         ` INSERT INTO Users (email, name, city) VALUES (? , ? , ?)`,[email,name,city]
         );
-
 console.log(">>>check results: ", results)
 res.send('Creat user success!  ')
 }
-
 
 const PostCreate = (req,res) => {
     res.render('create.ejs')
